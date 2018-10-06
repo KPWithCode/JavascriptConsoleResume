b50fa8a9b6037851b8053b3a80c3f05516e08765
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2018-10-06 03:52:00 -0400

    added functions and variables

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-let name = 'Kyle Phillips';
-console.log(name.toUpperCase());
-console.log('Career: Healthcare/Mental Health');
-console.log('Description: Passionate about learning and focused on growth and development. ');
-console.log('Interests: Videogames, Front End Development');
-console.log('My Previous Experience: Mental Health Therapy Aide From 07/2016 - 07/2018 Assist in implementation of Psycho Educational Model (PEM)1 to 1 observation of clients: within eyesight, 6 feet and arms length. Familiarizing oneself with trauma and diagnosis history to maintain patient sensitivity as well as keeping a log of new and recurring behavior.');
-console.log ('Van Wagner Sports & Entertainment. East Rutherford NJ Brand AmbassadorFrom 09/ 2013 - 2015.Positively reinforce consumer base for brand participation. Establish relationship between families and brand. Overall support of brand identity');
-console.log('My Skills: Problem Solving, Interpersonal Skills',' BAM: Mediocre CSS');
-
-
-function displayPosition (name, title, description)   {
-    console.log(name, title, description);
+function displayPosition( name, career, description) {
+    console.log(name.toUpperCase());
+    console.log(career);
+    console.log(description);
 }
+displayPosition ('Name: Kyle Phillips','Career: HealthCare/Mental Health', 'Description: Angry Teenagers');
+
+let interests = 'Interests: Video Games, Front End Development';
+console.log (interests);
+let workexperience = 'Van Wagner Sports & Entertainment. East Rutherford NJ Brand AmbassadorFrom 09/ 2013 - 2015.Positively reinforce consumer base for brand participation. Establish relationship between families and brand. Overall support of brand identity';
+console.log (workexperience);
+let skill = 'My Skills: Problem Solving, Interpersonal Skills, Moonwalking in Socks, & most importantly BAM: Mediocre CSS';
+
+let one = "True";
+let two = "False";
+
+function displaySkill (skill, one)  {
+    console.log(skill);
+}
+displaySkill(skill);
+
+
+
+console.log('');
+
+
+
+
+
 
