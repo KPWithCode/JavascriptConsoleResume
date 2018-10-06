25b3c26422498a1e58ff69b63e7b24a16142051c
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2018-10-06 14:17:37 -0400

    still dont know how to correctly apply core JS Concepts. Moving on for now

--- script.js
+++ script.js
@@ -9,20 +9,53 @@ let interests = 'Interests: Video Games, Front End Development';
 console.log (interests);
 let workexperience = 'Van Wagner Sports & Entertainment. East Rutherford NJ Brand AmbassadorFrom 09/ 2013 - 2015.Positively reinforce consumer base for brand participation. Establish relationship between families and brand. Overall support of brand identity';
 console.log (workexperience);
-let skill = 'My Skills: Problem Solving, Interpersonal Skills, Moonwalking in Socks, & most importantly BAM: Mediocre CSS';
 
-let one = "True";
-let two = "False";
 
-function displaySkill (skill, one)  {
-    console.log(skill);
+let mySkill = ['Walking on my Hands', 'Square Dancing in my underwear', 'Moonwalking in Socks', ' Writing mediocre CSS', 'hula hoop'];
+// return (mySkill[0].length);
+// return (mySkill[1].length);
+// return (mySkill[2].length);
+// return (mySkill[3].length);
+// return (mySkill[4].length);
+
+
+if (mySkill[0].length<15)   {
+    console.log('Walking on my Hands TRUE');
+}   else {
+    console.log('Walking on Hands?I GOTEEEM that was FALSE');
+
 }
-displaySkill(skill);
+
+if (mySkill[1].length<15)   {
+    console.log('Square Dancing in my underwear TRUE');
+}   else {
+    console.log('Me???Square Dance?? oh please');
+
+}
 
 
+if (mySkill[2].length<15)   {
+    console.log(' Moonwalking in Socks TRUE');
+}   else {
+    console.log('ME??? Moonwalk??? Yea Right');
 
-console.log('');
+}
 
+
+if (mySkill[3].length<15)   {
+    console.log(' Writing mediocre CSS TRUE');
+}   else {
+    console.log('CSS??? GOTEEEM that was FALSE');
+
+}
+
+
+if (mySkill[4].length<15)   {
+    console.log('hula hoop TRUE');
+}   else {
+    console.log('Hula Hoop??GOTEEEM that was FALSE');
+
+}
 
 
 
