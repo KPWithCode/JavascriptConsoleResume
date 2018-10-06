9283cbd3c770003a9dfd905066aaf2a875c02ed7
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2018-10-05 22:14:12 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log('Kyle Phillips')
+console.log('Healthcare/Mental Health')
+console.log('Passionate about learning and focused on growth and development. ')
+console.log('Videogames, Front End Development')
+console.log('Mental Health Therapy Aide From 07/2016 - 07/2018 Assist in implementation of Psycho Educational Model (PEM)1 to 1 observation of clients: within eyesight, 6 feet and arms length. Familiarizing oneself with trauma and diagnosis history to maintain patient sensitivity as well as keeping a log of new and recurring behavior.');
+console.log ('Van Wagner Sports & Entertainment. East Rutherford NJ Brand AmbassadorFrom 09/ 2013 - 2015.Positively reinforce consumer base for brand participation. Establish relationship between families and brand. Overall support of brand identity');
+console.log('Problem Solving, Interpersonal Skills',' BAM: Mediocre CSS')
