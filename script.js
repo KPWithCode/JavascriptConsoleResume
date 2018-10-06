a9fdbd0348e419b805ab19052173c57681f73046
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2018-10-05 22:38:28 -0400

    added displayPosition function

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-console.log('Kyle Phillips')
-console.log('Healthcare/Mental Health')
-console.log('Passionate about learning and focused on growth and development. ')
-console.log('Videogames, Front End Development')
-console.log('Mental Health Therapy Aide From 07/2016 - 07/2018 Assist in implementation of Psycho Educational Model (PEM)1 to 1 observation of clients: within eyesight, 6 feet and arms length. Familiarizing oneself with trauma and diagnosis history to maintain patient sensitivity as well as keeping a log of new and recurring behavior.');
+console.log('Name: Kyle Phillips');
+console.log('Career: Healthcare/Mental Health');
+console.log('Description: Passionate about learning and focused on growth and development. ');
+console.log('Interests: Videogames, Front End Development');
+console.log('My Previous Experience: Mental Health Therapy Aide From 07/2016 - 07/2018 Assist in implementation of Psycho Educational Model (PEM)1 to 1 observation of clients: within eyesight, 6 feet and arms length. Familiarizing oneself with trauma and diagnosis history to maintain patient sensitivity as well as keeping a log of new and recurring behavior.');
 console.log ('Van Wagner Sports & Entertainment. East Rutherford NJ Brand AmbassadorFrom 09/ 2013 - 2015.Positively reinforce consumer base for brand participation. Establish relationship between families and brand. Overall support of brand identity');
-console.log('Problem Solving, Interpersonal Skills',' BAM: Mediocre CSS')
+console.log('My Skills: Problem Solving, Interpersonal Skills',' BAM: Mediocre CSS');
+
+function displayPosition (name, title, description)   {
+    console.log(name, title, description);
+}
